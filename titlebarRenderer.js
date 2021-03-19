2f487bb65253d4a6b120a8f57528a41eabbdeec2
Sirzento <sirzento@gmx.de>
2021-03-19 18:47:54 +0100

    added more pages

--- titlebarRenderer.js
+++ titlebarRenderer.js
@@ -1,7 +1,24 @@
 const customTitlebar = require('custom-electron-titlebar');
+const ipcRenderer2 = require('electron').ipcRenderer;
 
 new customTitlebar.Titlebar({
 	backgroundColor: customTitlebar.Color.fromHex('#444'),
     maximizable: false,
     menu: null
 });
+
+ipcRenderer2.send('getUserInfo');
+
+ipcRenderer2.on('sendUserInfo', function (event, args) {
+  console.log(args[0]);
+  console.log(args[1]);
+  const userNameElement = document.getElementById("userName")
+  if (userNameElement) userNameElement.innerText = args[0];
+
+  const userImageElement = document.getElementById("userImage")
+  if (userImageElement) userImageElement.src = args[1];
+});
+
+function logout() {
+    ipcRenderer2.send('logout');
+}
